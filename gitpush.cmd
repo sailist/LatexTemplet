c48f42cf04c215984856fff38902585de6c7c99c
sailist <sailist@outlook.com>
2019-08-28 14:49:39 +0800

    2019年8月28日

--- gitpush.cmd
+++ gitpush.cmd
@@ -1,6 +1,6 @@
 set /p commitmsg=input commitmsg:
-git add preamble/*
-git add gitpush.cmd
-git add .gitignore
+git add *
+REM git add gitpush.cmd
+REM git add .gitignore
 git commit -m %commitmsg%
 git push
